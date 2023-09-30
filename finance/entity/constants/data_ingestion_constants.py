a7fd6aa068ecdec44709d3f291b4956754bca5f6
manish <manumi296@gmail.com>
2023-09-30 08:54:20 +0530

    data ingestion constants

---
+++ finance/entity/constants/data_ingestion_constants.py
@@ -0,0 +1,5 @@
+DATA_INGESTION_DIR = 
+DATA_INGESTION_START_DATE = 
+DATA_INGESTION_DATA_SOURCE_URL = f"https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/" \
+                                f"?date_received_max=<todate>&date_received_min=<fromdate>" \
+                                f"&field=all&format=json"
